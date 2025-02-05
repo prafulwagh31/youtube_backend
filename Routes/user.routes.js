9ca52497f282824b2a3b084e9873776e4ea44982
Praful Wagh <praful@kanexy.com>
2025-02-05 21:12:21 +0530

    update get user api

--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -1,6 +1,7 @@
-import { userLogin, userRegistration } from "../Controller/user.controller.js";
+import { getSingleUser, userLogin, userRegistration } from "../Controller/user.controller.js";
 
 export function userRoute(app){
     app.post("/youtube/register",userRegistration);
     app.post("/youtube/login", userLogin);
+    app.get("/youtube/:id", getSingleUser);
 }
